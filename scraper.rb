6a864619f918ba8a31e7d1fd5c86e5cf4e9e28ab
jandinter <jandinter@users.noreply.github.com>
2021-12-27 23:17:00 +0100

    Improve scraper

--- scraper.rb
+++ scraper.rb
@@ -1,39 +1,33 @@
-require 'down'
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'down/http'
 require 'nokogiri'
 require 'pathname'
 require 'uri'
 require 'zip'
 
-TOC_PATH = 'https://www.gesetze-im-internet.de/gii-toc.xml'.freeze
-HOST_NAME = 'https://www.gesetze-im-internet.de'.freeze
+TOC_PATH = 'https://www.gesetze-im-internet.de/gii-toc.xml'
+HOST_NAME = 'https://www.gesetze-im-internet.de'
 MAX_SIZE_FOR_FILES = 30 * 1024**2 # in MiB
-DOWNLOAD_FOLDER = 'gesetze'.freeze
-
-class Object
-  def logger
-    puts self
-    self
-  end
-end
+DOWNLOAD_FOLDER = 'gesetze'
 
 class Toc
   include Enumerable
 
+  attr_reader :entries
+
   def initialize(xml)
     @entries = Nokogiri::XML::Document
-    .parse(xml)
-    .search('items', 'item')
-    .select { |item| !item.at_xpath('link').nil? && !item.at_xpath('title').nil? }
-    .map do |item|
-      TocEntry.new(name: item.at_xpath('title').content,
-        uri: item.at_xpath('link').content)
-    end
+               .parse(xml)
+               .search('items', 'item')
+               .select { |item| !item.at_xpath('link').nil? && !item.at_xpath('title').nil? }
+               .map do |item|
+                 TocEntry.new(name: item.at_xpath('title').content,
+                              uri: item.at_xpath('link').content)
+               end
 
     puts @entries.count
-  end
-
-  def entries
-    @entries
   end
 end
 
@@ -43,7 +37,6 @@ class TocEntry
   def initialize(name:, uri:)
     @name = name
     @uri = URI.parse(uri)
-    @short_name
   end
 
   def short_name
@@ -51,26 +44,27 @@ class TocEntry
   end
 end
 
-toc_string = Down.download(TOC_PATH)
+toc_string = Down::Http.new.download(TOC_PATH)
 
 toc = Toc.new(toc_string)
 
 begin
+  count = 1
   toc.entries.each do |item|
-    puts item
-    zip_file = Down.download(item.uri, max_size: MAX_SIZE_FOR_FILES)
-    puts zip_file.size
-    file_name = File.join(Pathname.getwd, DOWNLOAD_FOLDER,
-                (Pathname.new(item.uri.path).dirname.to_s.delete('/').to_s +
-                '.xml'))
+    zip_file = Down::Http.download(item.uri)
+    file_name = File.join(Pathname.getwd,
+                          DOWNLOAD_FOLDER,
+                          "#{Pathname.new(item.uri.path).dirname.to_s.delete('/')}.xml")
+    puts file_name
     Zip::File.open(zip_file) do |files|
       Zip.on_exists_proc = true
       entry = files.glob('*.xml').first
       raise 'File too large when extracted' if entry.size > MAX_SIZE_FOR_FILES
 
       files.extract(entry, file_name)
+      count += 1
     end
   end
+ensure
+  puts "Files saved: #{count}"
 end
-
-
